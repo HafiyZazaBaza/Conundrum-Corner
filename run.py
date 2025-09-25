ee5ff03068bcc02998c4a9aa552fc9f227cb7d95
Irfan Mukhriz <meimam06@gmail.com>
2025-09-25 16:48:05 +0800

    Added more codes to deploy

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from conundrum import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, use_reloader=False, workers=3)
+
