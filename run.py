9f102dea9d7c62e0ee2b12a9f70fc6aa805d146d
Irfan Mukhriz <meimam06@gmail.com>
2025-09-25 16:58:21 +0800

    Added more codes to deploy

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-# run.py
+# conundrum/run.py
+
+import os
 from conundrum import create_app, socketio
 
 app = create_app()
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
