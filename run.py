fc2c5704d1ebf614cc5dccfd6a5523223a6880d4
Irfan Mukhriz <meimam06@gmail.com>
2025-09-26 10:10:36 +0800

    Rechanged code to run

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 # conundrum/run.py
 
-import os
-from conundrum import create_app, socketio
-
-app = create_app()
-
-port = int(os.environ.get("PORT", 5000))
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+from conundrum import create_app, socketio 
+app = create_app() 
+if __name__ == "__main__": 
+    socketio.run(app, debug=True)
